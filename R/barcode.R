44a08e7a0a8504987189763395d7dccfe5bf2c55
John W. Emerson <john.emerson@yale.edu>
2025-03-20 15:50:11 +0000

    version 1.4.0

--- R/barcode.R
+++ R/barcode.R
@@ -20,11 +20,11 @@
                       buffer=0.02,
                       log=FALSE,
                       outerbox=TRUE
-              ) {
-
+) {
+  
   #if (!require(grid)) stop("library(grid) is required and unavailable.\n\n")
   #if (!require(lattice)) stop("library(lattice) is required and unavailable.\n\n")
-
+  
   if (!is.null(labelloc)) {
     if (labelloc=="right" | labelloc=="top") labelloc <- FALSE
     if (labelloc=="left" | labelloc=="bottom") labelloc <- TRUE
@@ -33,70 +33,74 @@
     if (axisloc=="right" | axisloc=="top") axisloc <- FALSE
     if (axisloc=="left" | axisloc=="bottom") axisloc <- TRUE
   }
-
+  
   if (is.vector(x) && !is.list(x)) x <- list(x)
   if (is.null(names(x))) names(x) <- as.character(1:length(x))
-
+  
   if (is.matrix(x)) x <- as.data.frame(x)
-
+  
   if (newpage) grid.newpage()
   grid.text(main, 0.5, unit(1, "npc")-unit(1,"lines"), gp=gpar(fontface="bold"))
-
+  
   # If there is an axis at the top as well as a title, and the labels
   # are inside the viewport, make extra space:
-  if (!is.null(axisloc) && !axisloc && main!="" && !labelouter)
+  if (!is.null(axisloc) && main!="" && !labelouter)
     outer.margins$top <- outer.margins$top + unit(2, "lines")
-
+  
   xlaboffset <- unit(2.5, "lines")
   if (!is.null(axisloc) && xlab!="" && !labelouter) {
     if (axisloc) {
-      if (horizontal) outer.margins$bottom <- outer.margins$bottom + unit(1.5, "lines")
-      else outer.margins$top <- outer.margins$top + unit(1.5, "lines")
+      # Axis location for horizontal and vertical (changed 9/18/2024)
+      if (horizontal) {
+        outer.margins$bottom <- outer.margins$bottom + unit(1.5, "lines")
+      } else outer.margins$left <- outer.margins$left + unit(1.5, "lines")
     } else {
-      if (horizontal) outer.margins$top <- outer.margins$top + unit(1.5, "lines")
-      else {
-        outer.margins$left <- outer.margins$left + unit(1.5, "lines")
-        outer.margins$top <- outer.margins$top
-        outer.margins$right <- outer.margins$right - unit(1.5, "lines")
-      }
+      # No axis version plot margins (changed 9/18/2024)
+      if (horizontal) {
+        outer.margins$top <- outer.margins$top + unit(1.5, "lines")
+      } else outer.margins$right <- outer.margins$right + unit(1.5, "lines")
     }
   }
-    
-  if (horizontal) {
-    thisangle <- 0
-    thisjust <- c("left", "bottom")
-  } else {
-    thisangle <- 90
-    thisjust <- c("left", "top")
-    pushViewport(viewport(x=0, y=0,
-                          width=convertHeight(unit(1, "npc"), "inches"),
-                          height=convertWidth(unit(1, "npc"), "inches"),
-                          just=c("left", "bottom")))
-  }
+  
+  # Proposed REMOVAL OF THIS 9/18/2024
+  # if (horizontal) {
+  #   thisangle <- 0
+  #   thisjust <- c("left", "bottom")
+  # } else {
+  #   thisangle <- 90
+  #   thisjust <- c("left", "top")
+  #   pushViewport(viewport(x=0, y=0,
+  #                         width=convertHeight(unit(1, "npc"), "inches"),
+  #                         height=convertWidth(unit(1, "npc"), "inches"),
+  #                         just=c("left", "bottom")))
+  # }
   if (labelouter) {
     outer.margins=list(bottom=unit(0, "lines"),
-                                       left=unit(0, "lines"),
-                                       top=unit(0, "lines"),
-                                       right=unit(0, "lines"))
+                       left=unit(0, "lines"),
+                       top=unit(0, "lines"),
+                       right=unit(0, "lines"))
   }
-
+  
+  
   vp.main <- viewport(x=outer.margins$left,
                       y=outer.margins$bottom,
                       width=unit(1, "npc")-outer.margins$right-outer.margins$left,
                       height=unit(1, "npc")-outer.margins$top-outer.margins$bottom,
-                      just=thisjust, angle=thisangle,
+                      just=c("left","bottom"),
                       name="main", clip="off")
   pushViewport(vp.main)
   if (outerbox) grid.rect()
-
+  
   barcode.panel(x, horizontal=horizontal, nint=nint, xlim=xlim,
                 labelloc=labelloc, labelouter=labelouter,
                 fontsize=fontsize, ptsize=ptsize, bcspace=bcspace,
                 use.points=use.points, xlab=xlab, xlaboffset=xlaboffset,
                 axisloc=axisloc, buffer=0.02, log=log)
   popViewport(1)
-  if (!horizontal) popViewport(1)
-
+  
+  # Proposed removal 9/18/2024
+  # if (!horizontal) popViewport(1) 
+  
 }
 
 
--- R/barcode.panel.R
+++ R/barcode.panel.R
@@ -1,14 +1,15 @@
-"barcode.panel" <- function(x, horizontal=TRUE, xlim=NULL, labelloc=TRUE, axisloc=TRUE,
+"barcode.panel" <- function(x, horizontal=TRUE, xlim=NULL, labelloc=TRUE,
+                            axisloc=TRUE,
                             labelouter=FALSE, nint=0, fontsize=9, 
                             ptsize=unit(0.25, "char"), ptpch=1, bcspace=NULL,
                             xlab="", xlaboffset=unit(2.5, "lines"),
                             use.points=FALSE, buffer=0.02, log=FALSE) {
-
+  
   if (!is.list(x)) { stop("x must be a list") }
-
+  
   K <- length(x)
   for (i in 1:K) x[[i]] <- x[[i]][!is.na(x[[i]])]
-
+  
   # Figure out some global things we'll need for spacing:  
   maxct <- 0
   if (is.null(xlim)) {
@@ -18,28 +19,43 @@
     minx <- xlim[1]
     maxx <- xlim[2]
   }
+  # Left offset works as bottom offset for vertical plot (9/18/2024)
   xleftoffset <- unit(1, "strwidth", names(x)[1])
   for (i in 1:K) {
     y <- x[[i]]
     if (length(y)>0) {
-      if (nint>0) z <- hist(y, breaks=pretty(unlist(x), n=nint), plot=FALSE)$counts
+      if (nint>0) z <- hist(y, breaks=pretty(unlist(x), n=nint),
+                            plot=FALSE)$counts
       else z <- table(y)
       maxct <- max(maxct, max(z))
       xleftoffset <- max(xleftoffset, unit(1, "strwidth", names(x)[i]))
     }
   }
   maxct <- maxct + 3
-  if (log) { maxct <- log(maxct) }
-  xleftoffset <- 1.05*xleftoffset
+  if (log) maxct <- log(maxct)
+  xleftoffset <- 1.05 * xleftoffset
   if (is.null(labelloc) || !labelloc) {
     xrightoffset <- xleftoffset
     xleftoffset <- unit(0, "npc")
-    xtextloc <- unit(1, "npc") - xrightoffset
-    xtextalign <- "left"
+    # Factor location (9/18/2024)
+    if (horizontal) {
+      xtextloc <- unit(1, "npc") - xrightoffset
+      xtextalign <- "left"
+    } else {
+      xtextloc <- unit(1, "npc") - xrightoffset
+      xtextalign <- "right" 
+    }
+    
   } else {
     xrightoffset <- unit(0, "npc")
-    xtextloc <- xleftoffset
-    xtextalign <- "right"
+    # Flipped factor location (9/18/2024)
+    if (horizontal) {
+      xtextloc <- xleftoffset
+      xtextalign <- "right"
+    } else {
+      xtextloc <- unit(0, "npc") + xleftoffset
+      xtextalign <- "left" 
+    }
   }
   if (labelouter) {
     xleftoffset <- unit(0, "npc")
@@ -48,31 +64,58 @@
     else xtextloc <- unit(-0.02, "npc")
   }
   if (is.null(bcspace)) bcspace <- max(0.2, 1.5 / (maxct + 1))
-
+  
   # Trivial viewport to properly create axis labels.
-  pushViewport(viewport(x=xleftoffset, 
-                        y=unit(0, "npc"),
-                        width=unit(1, "npc")-xleftoffset-xrightoffset,
-                        height=unit(1, "npc"),
-                        xscale=c(minx, maxx),
-                        just=c("left", "bottom")))
-  if (!is.null(axisloc)) {
-    grid.xaxis(main=axisloc, gp=gpar(fontsize=fontsize))
-    if (axisloc) grid.text(xlab, x=unit(0.5, "npc"), y=unit(0, "npc") - xlaboffset)
-    else grid.text(xlab, x=unit(0.5, "npc"), y=unit(1, "npc") + xlaboffset)
+  if (horizontal) {
+    pushViewport(viewport(x=xleftoffset, 
+                          y=unit(0, "npc"),
+                          width=unit(1, "npc") - xleftoffset - xrightoffset,
+                          height=unit(1, "npc"),
+                          xscale=c(minx, maxx),
+                          just=c("left", "bottom")))
+    if (!is.null(axisloc)) {
+      grid.xaxis(main=axisloc, gp=gpar(fontsize=fontsize))
+      if (axisloc) grid.text(xlab, x=unit(0.5, "npc"),
+                             y=unit(0, "npc") - xlaboffset)
+      else grid.text(xlab, x=unit(0.5, "npc"), y=unit(1, "npc") + xlaboffset)
+    }
+  } else { 
+    # Vertical axis (9/18/2024)
+    pushViewport(viewport(x=unit(0, "npc"), 
+                          y=xleftoffset,
+                          width=unit(1, "npc"),
+                          height=unit(1, "npc") - xleftoffset - xrightoffset,
+                          yscale=c(minx, maxx),
+                          just=c("left", "bottom")))
+    if (!is.null(axisloc)) {
+      grid.yaxis(main=axisloc, gp=gpar(fontsize=fontsize))
+      if (axisloc) grid.text(xlab, x=unit(0, "npc") - xlaboffset,
+                             y=unit(0.5, "npc"),rot=90)
+      else grid.text(xlab, x=unit(1, "npc") + xlaboffset,
+                     y=unit(0.5, "npc"), rot = -90)
+    }
   }
+  
   popViewport(1)
-
+  
   # Now do each of the barcodes:
   for (i in 1:K) {
-
+    
     y <- x[[i]]
-
-    # Handle the factor labels:
-    if (!is.null(labelloc))
-      grid.text(names(x)[i], x=xtextloc, y=unit((i-1)/K, "npc")+0.5*unit(1/K, "npc"),
-                just=xtextalign, gp=gpar(fontsize=fontsize))
-
+    
+    # Handle the factor labels(horizontal/vertical) (9/18/2024):
+    if (!is.null(labelloc)) {
+      if (horizontal) {
+        grid.text(names(x)[i], x=xtextloc,
+                  y=unit((i-1)/K, "npc") + 0.5*unit(1/K, "npc"),
+                  just=xtextalign, gp=gpar(fontsize=fontsize))
+      } else {
+        grid.text(names(x)[i], x=unit((i-1)/K, "npc")+0.5*unit(1/K, "npc"),
+                  y=xtextloc,
+                  just=xtextalign, gp=gpar(fontsize=fontsize), rot = -90) 
+      }
+    }
+    
     if (nint>0) {
       zhist <- hist(y, breaks=pretty(unlist(x), n=nint), plot=FALSE)
       z <- zhist$counts
@@ -81,55 +124,96 @@
       z <- table(y)
       mids <- as.numeric(names(z))
     }
-
+    
     if (length(mids)>0) {
-
-      # The barcode part of things:
-      vp.barcode <- viewport(x=xleftoffset,
-                             y=unit((i-1)/K, "npc") + unit(0.05/K, "npc"),
-                             width=unit(1, "npc")-xleftoffset-xrightoffset,
-                             height=unit(1/K, "npc")*bcspace - unit(0.05/K, "npc"),
-                             xscale=c(minx, maxx), yscale=c(0,1),
-                             just=c("left", "bottom"),
-                             name="barcode", clip="off")
-      pushViewport(vp.barcode)
-      grid.segments(unit(mids[z>0], "native"), 0, unit(mids[z>0], "native"), 1)
-      popViewport(1)
-
-      # The histogram part of things:
-      vp.hist <- viewport(x=xleftoffset,
-                          y=unit((i-1)/K, "npc")+unit(1/K, "npc")*bcspace,
-                          width=unit(1, "npc")-xrightoffset-xleftoffset,
-                          height=unit(1/K, "npc")-unit(1/K, "npc")*bcspace,
-                          xscale=c(minx, maxx), yscale=c(0,1),
-                          just=c("left", "bottom"),
-                          name="hist", clip="off")
-      pushViewport(vp.hist)
-      vp.buffer <- viewport(x=0, y=0.05, width=1, height=0.9, just=c("left", "bottom"),
-                            xscale=c(minx, maxx), yscale=c(0,1))
-      pushViewport(vp.buffer)
-
+      
+      # The barcode part of things(horizontal/vertical):
+      if (horizontal) {
+        vp.barcode <- viewport(x=xleftoffset,
+                               y=unit((i-1)/K, "npc") + unit(0.05/K, "npc"),
+                               width=unit(1, "npc")-xleftoffset-xrightoffset,
+                               height=unit(1/K, "npc")*bcspace - unit(0.05/K, "npc"),
+                               xscale=c(minx, maxx), yscale=c(0,1),
+                               just=c("left", "bottom"),
+                               name="barcode", clip="off")
+        pushViewport(vp.barcode)
+        grid.segments(unit(mids[z>0], "native"), 0, unit(mids[z>0], "native"), 1)
+        popViewport(1)
+        
+        # The histogram part of things:
+        vp.hist <- viewport(x=xleftoffset,
+                            y=unit((i-1)/K, "npc")+unit(1/K, "npc")*bcspace,
+                            width=unit(1, "npc")-xrightoffset-xleftoffset,
+                            height=unit(1/K, "npc")-unit(1/K, "npc")*bcspace,
+                            xscale=c(minx, maxx), yscale=c(0,1),
+                            just=c("left", "bottom"),
+                            name="hist", clip="off")
+        pushViewport(vp.hist)
+        vp.buffer <- viewport(x=0, y=0.05, width=1, height=0.9, 
+                              just=c("left", "bottom"),
+                              xscale=c(minx, maxx), yscale=c(0,1))
+        pushViewport(vp.buffer)
+        
+      } else { 
+        # Vertical barcode and histogram (9/18/2024)
+        vp.barcode <- viewport(x=unit((i-1)/K, "npc") + unit(0.05/K, "npc"),
+                               y=xleftoffset,
+                               width=unit(1/K, "npc")*bcspace - unit(0.05/K, "npc"),
+                               height=unit(1, "npc") - xleftoffset - xrightoffset,
+                               xscale=c(0,1), yscale=c(minx,maxx),
+                               just=c("left", "bottom"),
+                               name="barcode", clip="off")
+        pushViewport(vp.barcode)
+        grid.segments(0,unit(mids[z>0], "native"), 1,unit(mids[z>0], "native"))
+        popViewport(1)
+        
+        # The histogram part of things:        
+        vp.hist <- viewport(x=unit((i-1)/K, "npc")+unit(1/K, "npc")*bcspace,
+                            y=xleftoffset,
+                            width=unit(1/K, "npc") - unit(1/K, "npc")*bcspace,
+                            height=unit(1, "npc") - xleftoffset - xrightoffset,
+                            xscale=c(0,1), yscale=c(minx,maxx),
+                            just=c("left", "bottom"),
+                            name="hist", clip="off")
+        pushViewport(vp.hist)
+        vp.buffer <- viewport(x=0.05, y=0, width=0.9, height=1, just=c("left", "bottom"),
+                              xscale=c(0,1), yscale=c(minx,maxx))
+        pushViewport(vp.buffer)
+      }
+      
+      
+      
       for (j in 1:length(z)) {
         if (z[j]>1) {
           xx <- rep(mids[j], z[j]-1)
           if (log) {
             yy <- ( log(2 + 1:(z[j]-1)) ) / maxct
           } else { yy <- (1:(z[j]-1))/maxct }
-          if (use.points) grid.points(unit(xx, "native"), yy, pch=ptpch, size=ptsize)
-          else {
-            if (log) {
-              yy <- c(yy, log(2 + z[j]) / maxct)
-            } else { yy <- c(yy, (z[j])/maxct) }
-            grid.segments(unit(mids[j], "native"), unit(1/maxct, "npc"),
-                               unit(mids[j], "native"), unit(max(yy), "npc"))
+          
+          if (use.points) {
+            # Point plot for horizontal and vertical (9/18/2024)
+            if (horizontal) grid.points(unit(xx, "native"), yy, pch=ptpch, size=ptsize)
+            else grid.points(yy,unit(xx, "native"), pch=ptpch, size=ptsize)
+          } else {
+            if (log) yy <- c(yy, log(2 + z[j]) / maxct)
+            else yy <- c(yy, (z[j])/maxct)
+            
+            # Grid segments horizontal and vertical (9/18/2024)
+            if (horizontal) {
+              grid.segments(unit(mids[j], "native"), unit(1/maxct, "npc"),
+                            unit(mids[j], "native"), unit(max(yy), "npc"))
+            } else {
+              grid.segments(unit(1/maxct, "npc"),unit(mids[j], "native"),
+                            unit(max(yy), "npc"), unit(mids[j], "native"))
+            }
           }
         }
       }
-
+      
       popViewport(2)
-
+      
     }
-
+    
   }
-
+  
 }
